223a510441c17539f033993139c5480c5d49f252
yakmax <yakmax@gmail.com>
2020-11-17 16:30:31 +0500

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking
+{
+    class Car
+    {
+
+        public string number { get; set; }
+        // госномер
+        public string color { get; set; } 
+        // цвет
+        public string owner { get; set; }
+        // фамилия владельца
+        public int place { get; set; }
+        // место
+        public bool isparked { get; set; } //признак присутствия на стоянке.
+
+        public Car(string number, string color, string owner, int place, bool isparked)
+        //public Car(string number, string color)
+        {
+            //конструктор
+            this.number = number;
+            this.color = color;
+            this.owner = owner;
+            this.place = place;
+            this.isparked = isparked;
+            
+        }
+    }
+}
---
+++ Parking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking
+{
+    class Parking
+    {
+        
+        private Car[] places = new Car[100];
+        
+        public Car this[int index]
+        {
+            get
+            {
+                return places[index];
+            }
+            set
+            {
+                places[index] = value;
+            }
+        }
+
+    }
+}
